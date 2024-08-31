89440ff479fc61dd4a091cade0fe3ae8d4e48f11
AdityaShuk1a <adityasarika0292004@gmail.com>
2024-08-31 21:14:04 +0530

    created opening of file and reading of files

--- nevernote/app.py
+++ nevernote/app.py
@@ -1,2 +1,16 @@
 import numpy as np
 
+#opening file
+
+with open(files, "+a") as file:
+    while a!="1":
+        a = input()
+        file.writelines(a + '\n')
+        
+# reading data
+
+with open(files, "r") as file:
+    lines = file.readlines()
+    for line in lines:
+        print(line, end="")
+        
