a608a6de63228cafc1e27542eea8cf40da330622
AdityaShuk1a <adityasarika0292004@gmail.com>
2024-08-31 22:33:38 +0530

    craeted a loop to select the existing files

--- nevernote/app.py
+++ nevernote/app.py
@@ -1,40 +1,10 @@
 import numpy as np
 import os
 
-def addFile( a):
-    with open("pvt_file.txt", "a") as file:
-        file.write(a + ",")
-
-    with open("pvt_file.txt", '+a') as file:
-        while(a!="\n"):
-            file.write(a+",")
-
-collection= np.array([],dtype='S')
-
-if( os.path.getsize("pvt_file.txt")==0 ):
-    a=input("Enter file name: ")
-    addFile(a)
-
-else:
-    while askuser!=0:
-        askuser = int(input("Enter 1 to choose from existing file: \nEnter 2 for adding a new file: \nEnter 0 for exit"))
-        if(askuser == 1):
-            readFile("pvt_file.txt")
-                        
-        elif(askuser == 2):
-            addfile(a)
-            
-        else:
-            print("wrong input Enter again!")
-            continue
-
-    
-            
-    collection=np.append(collection,a)
 def enterData(files):
     
-    print("write in nevernote:\n")
     #opening file
+    print("write in nevernote:\n")
     
     with open(files, "+a") as file:
         while a!="X":
@@ -43,6 +13,7 @@ def enterData(files):
             file.writelines(a + '\n')
 
 def readData(files):
+    
     # reading data
     print("data\n")
 
@@ -50,6 +21,52 @@ def readData(files):
         lines = file.readlines()
         for line in lines:
             print(line, end="")
+
+def addFile( a):
+    with open("pvt_file.txt", "a") as file:
+        file.write(a + ",")
+
+    with open("pvt_file.txt", '+a') as file:
+        while(a!="\n"):
+            file.write(a+",")
+
+def readFile(s):
+    with open(s, "r") as file:
+        lines = file.readlines()
+        lines = lines.split(",")
+    return np.array(lines)
+    
+
+collection= np.array([],dtype='S')
+
+if( os.path.getsize("pvt_file.txt")==0 ):
+    a=input("Enter file name: ")
+    addFile(a)
+
+else:
+    
+    while (askuser!=0 or collection.size !=0):
+        askuser = int(input("Enter 1 to choose from existing file: \nEnter 2 for adding a new file: \nEnter 0 for exit"))
+        if(askuser == 1):
+            collection = readFile("pvt_file.txt")
+                        
+        elif(askuser == 2):
+            a=input("Enter file name: ")
+            addFile(a)
+            
+        else:
+            print("wrong input Enter again!")
+            continue
+    if (collection.size!=0){
+        for i in np.nditer(collection):
+            print(i)
+    }
+    file_index = int(input("Enter the file number you want to open: "))
+    files = collection[file_index]
+    enterData(files)
+    readData(files)    
+            
+    
     
     
 
