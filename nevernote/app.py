2ea3bd732f8dbacbde564f70bbaae37381c3c26f
AdityaShuk1a <adityasarika0292004@gmail.com>
2024-08-31 23:04:07 +0530

    compiled all the function and executed the main note file

--- nevernote/app.py
+++ nevernote/app.py
@@ -2,74 +2,62 @@ import numpy as np
 import os
 
 def enterData(files):
-    
-    #opening file
-    print("write in nevernote:\n")
-    
-    with open(files, "+a") as file:
-        while a!="X":
+    # Opening file
+    print("Write in nevernote:\n")
+    with open(files, "a") as file:
+        while True:
             print("Enter X to exit: \n")
             a = input()
+            if a == "X":
+                break
             file.writelines(a + '\n')
 
 def readData(files):
-    
-    # reading data
-    print("data\n")
-
+    # Reading data
+    print("Data\n")
     with open(files, "r") as file:
         lines = file.readlines()
         for line in lines:
             print(line, end="")
 
-def addFile( a):
+def addFile(a):
     with open("pvt_file.txt", "a") as file:
         file.write(a + ",")
 
-    with open("pvt_file.txt", '+a') as file:
-        while(a!="\n"):
-            file.write(a+",")
-
 def readFile(s):
-    with open(s, "r") as file:
-        lines = file.readlines()
-        lines = lines.split(",")
-    return np.array(lines)
-    
+    with open(s, 'r') as file:
+        lines = file.read().split(",")
+        print(lines)
+    return lines
 
-collection= np.array([],dtype='S')
+myFiles = "pvt_file.txt"
 
-if( os.path.getsize("pvt_file.txt")==0 ):
-    a=input("Enter file name: ")
+# Check if the file is empty and add a new file if it is
+if os.path.getsize(myFiles) == 0:
+    a = input("Enter file name: ")
     addFile(a)
 
-else:
-    
-    while (askuser!=0 or collection.size !=0):
-        askuser = int(input("Enter 1 to choose from existing file: \nEnter 2 for adding a new file: \nEnter 0 for exit"))
-        if(askuser == 1):
-            collection = readFile("pvt_file.txt")
-                        
-        elif(askuser == 2):
-            a=input("Enter file name: ")
-            addFile(a)
-            
-        else:
-            print("wrong input Enter again!")
-            continue
-    if (collection.size!=0){
-        for i in np.nditer(collection):
-            print(i)
-    }
-    file_index = int(input("Enter the file number you want to open: "))
-    files = collection[file_index]
-    enterData(files)
-    readData(files)    
-            
-    
-    
-    
+readfile = ""
+while True:
+    askuser = int(input("Enter 1 to choose from existing file: \nEnter 2 for adding a new file: \nEnter 0 for exit: "))
+    if askuser == 1:
+        readfile = readFile(myFiles)
+    elif askuser == 2:
+        a = input("Enter file name: ")
+        addFile(a)
+    elif askuser == 0:
+        print("Exiting....")
+        break
+    else:
+        print("Wrong input. Enter again!")
+        continue
 
+collection = np.array(readfile, dtype='S')
+if collection.size != 0:
+    for i in np.nditer(collection):
+        print(i)
 
-        
-        
+file_index = int(input("Enter the file number you want to open: "))
+files = collection[file_index].decode('utf-8')
+enterData(files)
+readData(files)
