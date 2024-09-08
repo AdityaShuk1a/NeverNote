5dad09564bb76668b44659cd88265bd643ad4275
AdityaShuk1a <adityasarika0292004@gmail.com>
2024-09-08 19:27:42 +0530

    added neon tech databse for better connection

--- nevernote/access_control.py
+++ nevernote/access_control.py
@@ -29,10 +29,10 @@ def saveInfo():
     try:
 
         conn = psycopg2.connect(
-            host = 'localhost',
-            dbname = 'NeverNote',
-            user = 'postgres',
-            password = 'kalisto',
+            host = 'ep-lucky-bush-a1g2oytk.ap-southeast-1.aws.neon.tech',
+            dbname = 'neondb',
+            user = 'neondb_owner',
+            password = 'YVE32adcAbvi',
             port = 5432
 
         )
@@ -61,49 +61,15 @@ def saveInfo():
 
 def loadInfo(uname, passwd):
     
-    conn = None
-    cur = None
-
-
-    try:
-
-        conn = psycopg2.connect(
-            host = 'localhost',
-            dbname = 'NeverNote',
-            user = 'postgres',
-            password = 'kalisto',
-            port = 5432
-
-        )
-        cur = conn.cursor()
-        create_table = '''create table if not exists SignUp(
-                            name varchar(100) not null,
-                            password varchar(100) not null)'''
-        cur.execute(create_table)
-        conn.commit()
-
-        first_query = '''INSERT INTO SignUp (Name, Password) values(%s,%s)'''
-        insertValues = (user, pswd)
-        cur.execute(first_query, insertValues)
-        conn.commit()
-        print("Data inserted successfully")
-
-    except psycopg2.DatabaseError as error:
-        print(error)
-    finally:
-        if cur is not None:
-            cur.close()
-        if conn is not None:
-            conn.close()
             
             
-    # with open("usernames.txt", 'r') as file1, open("password.txt",'r')as file2:
-    #     user_arr=np.loadtxt("usernames.txt",dtype=str)
-    #     pswd_arr=np.loadtxt("password.txt",dtype=str)
+    with open("usernames.txt", 'r') as file1, open("password.txt",'r')as file2:
+        user_arr=np.loadtxt("usernames.txt",dtype=str)
+        pswd_arr=np.loadtxt("password.txt",dtype=str)
 
-    # if uname in user_arr and passwd in pswd_arr :
+    if uname in user_arr and passwd in pswd_arr :
 
-    #     print("successfully loged in")
-    # else:
-    #     print("Sorry, Username does not exist -_-\n~signup~")
-    #     saveInfo()
+        print("successfully loged in")
+    else:
+        print("Sorry, Username does not exist -_-\n~signup~")
+        saveInfo()
