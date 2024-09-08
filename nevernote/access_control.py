1a16c4b27ea3edbda27889b5758c206a3b189634
AdityaShuk1a <adityasarika0292004@gmail.com>
2024-09-08 19:45:20 +0530

    created a seperate file for my database

--- nevernote/access_control.py
+++ nevernote/access_control.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+from db_connection import db_connect
 import psycopg2
 
 
@@ -19,43 +19,10 @@ def saveInfo():
     user =input("Enter the username: ")
     pswd=input("Enter the password: ")
     
-    # with open("usernames.txt", 'a') as file1, open("password.txt",'a')as file2:
-    #     file1.write(user+"\n")
-        #     file2.write(pswd+"\n")
-    conn = None
-    cur = None
-
-
-    try:
-
-        conn = psycopg2.connect(
-            host = 'ep-lucky-bush-a1g2oytk.ap-southeast-1.aws.neon.tech',
-            dbname = 'neondb',
-            user = 'neondb_owner',
-            password = 'YVE32adcAbvi',
-            port = 5432
-
-        )
-        cur = conn.cursor()
-        create_table = '''create table if not exists SignUp(
-                            name varchar(100) not null,
-                            password varchar(100) not null)'''
-        cur.execute(create_table)
-        conn.commit()
-
-        first_query = '''INSERT INTO SignUp (Name, Password) values(%s,%s)'''
-        insertValues = (user, pswd)
-        cur.execute(first_query, insertValues)
-        conn.commit()
-        print("Data inserted successfully")
-
-    except psycopg2.DatabaseError as error:
-        print(error)
-    finally:
-        if cur is not None:
-            cur.close()
-        if conn is not None:
-            conn.close()
+   
+    query = 'insert into signup(name, password) values(%s,%s)'
+    values = (user, pswd)
+    db_connect(query, values)
 
 
 
---
+++ nevernote/db_connection.py
@@ -0,0 +1,38 @@
+import psycopg2
+
+def db_connect(query, value=None):
+    conn = None
+    cur = None
+
+
+    try:
+
+        conn = psycopg2.connect(
+            host = 'ep-lucky-bush-a1g2oytk.ap-southeast-1.aws.neon.tech',
+            dbname = 'neondb',
+            user = 'neondb_owner',
+            password = 'YVE32adcAbvi',
+            port = 5432
+
+        )
+        cur = conn.cursor()
+        
+        if(value == None):
+            cur.execute(query)
+        else:
+            cur.execute(query, value)
+
+        # first_query = '''INSERT INTO SignUp (Name, Password) values(%s,%s)'''
+        # insertValues = (user, pswd)
+        
+        # cur.execute(first_query, insertValues)
+        conn.commit()
+        print("Data inserted successfully")
+
+    except psycopg2.DatabaseError as error:
+        print(error)
+    finally:
+        if cur is not None:
+            cur.close()
+        if conn is not None:
+            conn.close()
