5afd1f4802c9f67e5da1f8644280bad8fadd292c
anugyajais <jaiswalanugya09.aj@gmail.com>
2024-10-08 19:36:32 +0530

    added NotesModel and serializer , Updated API Views

--- NeverNote/api/models.py
+++ NeverNote/api/models.py
@@ -1,12 +1,9 @@
 from django.db import models
-
-# Create your models here.
 from django.db import models
 import uuid
 from datetime import datetime
 from django.core.validators import validate_email
 
-# Create your models here.
 
 def current_timestamp():
     return int(datetime.now().timestamp())
@@ -14,12 +11,25 @@ def current_timestamp():
 class UserModel(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user_name=models.CharField( max_length=50, null=False)
-    user_email = models.EmailField(validators=[validate_email], null=False)
-    user_phone_number=models.BigIntegerField(null=False)
+    user_email = models.EmailField(validators=[validate_email], null=False, unique=True)
+    user_phone_number=models.BigIntegerField(null=False, unique=True)
     timestamp = models.BigIntegerField(default=current_timestamp, editable=False)
     user_password = models.CharField(max_length=25)
     
-    
-    
-#no changes, just checking
+class NotesModel(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(UserModel, on_delete=models.CASCADE , related_name= 'notes')
+    note_heading = models.CharField(max_length=255, blank=True)
+    note_body= models.TextField(null=False)
+    created_at_timestamp = models.BigIntegerField(default=current_timestamp, editable=False)
+    updated_at_timestamp = models.BigIntegerField(default=current_timestamp, editable=False)
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.created_at_timestamp = current_timestamp()
+        self.updated_at_timestamp = current_timestamp()
+        self.note_heading=self.note_body[:60]
+        super(NotesModel, self).save(*args, **kwargs)
+    class Meta:
+        ordering= ['-updated_at_timestamp']
 
